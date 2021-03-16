6a8141706a6e01e584b7d2081524a82508747263
Brett Ritter <b.ritter@neu.edu>
2021-03-15 17:45:41 -0700

    Adds spa login and polling samples, Mar 15 notes and assignment

---
+++ samples/spa-login/public/todo.js
@@ -0,0 +1,183 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/services.js":
+/*!*************************!*\
+  !*** ./src/services.js ***!
+  \*************************/
+/*! namespace exports */
+/*! export checkLoginStatus [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export performLogin [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "checkLoginStatus": () => /* binding */ checkLoginStatus,
+/* harmony export */   "performLogin": () => /* binding */ performLogin
+/* harmony export */ });
+var checkLoginStatus = function checkLoginStatus() {
+  return fetch('/session', {
+    method: 'GET'
+  })["catch"](function () {
+    return Promise.reject({
+      error: 'network-error'
+    });
+  }).then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+
+    return response.json().then(function (err) {
+      return Promise.reject(err);
+    });
+  });
+};
+var performLogin = function performLogin(username) {
+  return fetch('/session', {
+    method: 'POST',
+    headers: new Headers({
+      'content-type': 'application/json'
+    }),
+    body: JSON.stringify({
+      username: username
+    })
+  })["catch"](function () {
+    return Promise.reject({
+      error: 'network-error'
+    });
+  }).then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+
+    return response.json().then(function (err) {
+      return Promise.reject(err);
+    });
+  });
+};
+
+/***/ }),
+
+/***/ "./src/todo.js":
+/*!*********************!*\
+  !*** ./src/todo.js ***!
+  \*********************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _services__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./services */ "./src/services.js");
+
+var todos = [];
+addLogin(); // Check for login
+
+(0,_services__WEBPACK_IMPORTED_MODULE_0__.checkLoginStatus)().then(function (userInfo) {
+  showContent();
+})["catch"](function (error) {
+  showLogin();
+});
+
+function showContent() {
+  document.querySelector('#todo-app .login').classList.add('hidden');
+  document.querySelector('#todo-app .logged-in').classList.remove('hidden');
+}
+
+function showLogin() {
+  document.querySelector('#todo-app .login').classList.remove('hidden');
+  document.querySelector('#todo-app .logged-in').classList.add('hidden');
+}
+
+function addLogin() {
+  document.querySelector('#todo-app .login button').addEventListener('click', function () {
+    var usernameEl = document.querySelector('#todo-app .login input');
+    var username = usernameEl.value; // call service
+
+    (0,_services__WEBPACK_IMPORTED_MODULE_0__.performLogin)(username).then(function (userInfo) {
+      showContent();
+      todos = userInfo.todos; // rerender?
+
+      renderTodos(todos);
+    })["catch"](function (err) {
+      // fixme
+      console.log(err);
+    });
+  });
+}
+
+function renderTodos(todos) {
+  var listEl = document.querySelector('#todo-app .todos');
+  var html = todos.map(function (todo) {
+    return "<li class=\"todo\"><span>".concat(todo.task, "</span></li>");
+  }).join("\n");
+  listEl.innerHTML = html;
+}
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/todo.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
+//# sourceMappingURL=todo.js.map
